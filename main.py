a3b6d89c2a1b12de188c1d6a0bfdf98245827092
CherePapa <pen.aaa@mail.ru>
2025-05-03 02:17:41 +0400

    main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
 
     # Определение преобразований для данных
     transform = transforms.Compose([
-        transforms.Resize((128, 128)),
+        transforms.Resize((28, 28)),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
